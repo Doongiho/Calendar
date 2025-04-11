051dd7db59a21e5a6972554daafd0b8b097443c5
seojiho <happyluck0426@gmail.com>
2025-04-11 17:16:21 +0900

    styles:달력 기본 구조 및 스타일 설정

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import MyCalendar from './pages/Main/MyCalendar'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/MyCalendar/:userId" element={<MyCalendar />} />
       </Routes>
     </BrowserRouter>
   );
--- front/src/pages/Header/Header.jsx
+++ front/src/pages/Header/Header.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "./Header.css";
 
 export default function Header() {
-
   return (
     <>
+      <header>
+        {/* 모바일 버전 */}
+        <div className="mo-header">
+          <div className="mobile-title">서지호님의 개인 일정표</div>
+          <div className="mobile-submenu submenu">
+            <div className="mo-room">
+              <span className="material-symbols-outlined">menu</span>
+            </div>
+          </div>
+        </div>
+        {/* PC 버전 */}
+        <div className="pc-header"> 
+          <div className="pc-title">서둥이님의 개인 일정표</div>
+          <div className="pc-submenu submenu">
+            <div className="alarm">
+              <span className="material-symbols-outlined">notifications</span>
+            </div>
+            <div className="mypage">
+              <span className="material-symbols-outlined">account_circle</span>
+            </div>
+            <div className="room">
+              <span className="material-symbols-outlined">menu</span>
+            </div>
+        </div>
+        </div>
+      </header>
     </>
-  )
+  );
 }
